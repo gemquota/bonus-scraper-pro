6e77b5f0a937f021ac2b12a3f552b3315350b705
RSIS Test <rsis@test.local>
2026-07-31 19:35:24 +1000

    Add error logging to serverless handler

--- api/index.js
+++ api/index.js
@@ -4,7 +4,15 @@ const createApp = require('../src/index');
 let appPromise = null;
 
 module.exports = async function handler(req, res) {
-  if (!appPromise) appPromise = createApp();
-  const app = await appPromise;
-  return app(req, res);
+  try {
+    if (!appPromise) appPromise = createApp();
+    const app = await appPromise;
+    return app(req, res);
+  } catch (err) {
+    console.error('HANDLER ERROR:', err && err.stack || err);
+    if (!res.headersSent) {
+      res.statusCode = 500;
+      res.end('Internal Server Error: ' + (err && err.message));
+    }
+  }
 };
